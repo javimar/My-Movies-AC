2a86974d9a24dad8d4816aef9db70fac2d21ec8c
javimar <javimardeveloper@gmail.com>
2021-02-21 18:53:24 +0100

    Add unit test for fav movies

---
+++ usecases/src/test/java/eu/javimar/usecases/mocks.kt
@@ -0,0 +1,18 @@
+package eu.javimar.mymoviesac.usecases
+
+import eu.javimar.domain.Movie
+
+internal val mockedMovie = Movie(
+    0,
+    "Casablanca",
+    "La pelicula va de bla bla",
+    "01/01/1942",
+    "",
+    "",
+    "EN",
+    "Casablanca",
+    9.0,
+    9.1,
+    true,
+    true
+)

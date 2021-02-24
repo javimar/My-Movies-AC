3ef772e36faf763fdf907fdbb91a079477bf2250
JaviMar <javimardeveloper@gmail.com>
2021-02-24 17:23:04 +0100

    Add worker test

---
+++ app/src/androidTest/java/eu/javimar/mymoviesac/background/RefreshDataWorkTest.kt
@@ -0,0 +1,35 @@
+package eu.javimar.mymoviesac.background
+
+import android.content.Context
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.work.ListenableWorker.Result
+import androidx.work.testing.TestListenableWorkerBuilder
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.component.KoinApiExtension
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class RefreshDataWorkTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    @KoinApiExtension
+    @Test
+    fun testSleepWorker() {
+        val worker = TestListenableWorkerBuilder<RefreshDataWork>(context).build()
+        runBlocking {
+            val result = worker.doWork()
+            assertThat(result, `is`(Result.success()))
+        }
+    }
+}
